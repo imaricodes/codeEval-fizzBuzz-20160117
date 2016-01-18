088db6cdcfb5a2e57bfca459ec5e1b355bf851b8
imaricodes <imaricodes@gmail.com>
2016-01-17 21:51:57 -0500

    code eval solved and submitted

--- codeEval-fizzBuzz-20160117/Program.cs
+++ codeEval-fizzBuzz-20160117/Program.cs
@@ -10,13 +10,22 @@ namespace codeEval_fizzBuzz_20160117
     {
         static void Main(string[] args)
         {
+            
+            //SUCCESS! complete solve.
 
-            int x = 2; //first factor
-            int y = 3; //second factor
-            int countTo = 12; //this will later be parsed from the string of three numbers given..what number to count up to
+            string line = "10 5 50"; //input... do not include this line in codeval submission          
+            string[] input = line.Split(' '); // Split string based on spaces.This will separate "string line" into separate words and assgin them to an array called "input"
+            int x = Convert.ToInt16(input[0]); // converts an item with index [0] (first word) from the array "input" to an interger and assigns to variable "o" ..repeated below
+            int y = Convert.ToInt16(input[1]);
+            int n = Convert.ToInt16(input[2]);
 
 
-            for (int i = 1; i <= countTo; i++)
+            //int x = 2; //first factor
+            //int y = 3; //second factor
+            //int countTo = 12; //this will later be parsed from the string of three numbers given..what number to count up to
+
+
+            for (int i = 1; i <= n; i++)
             {
                 //setting condition based on value of i
                 bool condition1 = (i % x == 0 && i % y != 0);  //true when x is divibisble by the seriesCount var and y is not
@@ -33,9 +42,10 @@ namespace codeEval_fizzBuzz_20160117
                 else
                     Console.Write(i + " ");
             }
+            Console.WriteLine();
 
 
-            Console.ReadKey();
+            Console.ReadKey(); //do not include in codeEval submission
         }
     }
 }
